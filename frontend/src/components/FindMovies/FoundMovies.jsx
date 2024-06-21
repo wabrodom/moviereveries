f08b719cf11055ab51a7566f4b83149937960cae
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-06-21 08:01:24 +0700

    implement debounce on search movies to relife some server load

--- frontend/src/components/FindMovies/FoundMovies.jsx
+++ frontend/src/components/FindMovies/FoundMovies.jsx
@@ -1,20 +1,34 @@
-import { useQuery } from '@apollo/client'
+import { useCallback, useEffect } from 'react'
+import { useLazyQuery } from '@apollo/client'
 import { FIND_MOVIES } from '../../queries'
-import FoundMoviesContainer from './FoundMoviesContainer';
+import FoundMoviesContainer from './FoundMoviesContainer'
+import { debounce } from '../../utils/helper'
 
-const FoundMovies = ( { text } ) => {
-  const searchFiltered = useQuery(FIND_MOVIES, {
-    variables: { text }, 
-  })
 
-  if (searchFiltered.loading) {
-    return <div>loading...</div>
-  }
+const FoundMovies = ({ text }) => {
+    const [searchMovies, { loading, data }] = useLazyQuery(FIND_MOVIES)
 
-  const foundMoviesFiltered = searchFiltered.data.findMovies
+    const debounceTextUpdate = useCallback(debounce(searchMovies, 500), [])
 
-  return (
-    <FoundMoviesContainer foundMoviesFiltered={foundMoviesFiltered} />
+    useEffect(() => {
+      debounceTextUpdate({ 
+        variables: { text },
+      })
+    }, [text])
+
+    /* 
+    after component rendered the first time, 
+    useEffect ran, a lazy query is ready to do it after setTimeout fulfill
+    loading is false, data is undefined
+    */
+    if (loading || data === undefined) {
+      return <div>loading...</div>
+    }
+
+    const foundMoviesFiltered = data.findMovies
+
+    return (
+      <FoundMoviesContainer foundMoviesFiltered={foundMoviesFiltered} />
   )
 }
 
