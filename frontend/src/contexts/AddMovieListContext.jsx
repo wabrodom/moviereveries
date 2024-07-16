71ff9f9e9933a78e62d9accc001caad720a2a44e
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-07-16 20:37:03 +0700

    make context for array of movie list

---
+++ frontend/src/contexts/AddMovieListContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from 'react'
+
+const AddMovieListContext = createContext()
+
+export const useAddMovieList = () => useContext(AddMovieListContext)
+
+export const AddMovieListContextProvider = ({ children }) => {
+  const initialState = [
+    {
+      "movieId": "",
+      "impression": "",
+      "imdb_id": "",
+      "original_title": "",
+      "primary_title": "",
+    }
+  ]
+
+  const [movieList, setMovieList] = useState([])
+
+  return (
+    <AddMovieListContext.Provider value={{ movieList, setMovieList }}>
+      {children}
+    </AddMovieListContext.Provider>
+  )
+}
+
+
+/*
+  "list": [
+    {
+      "movieId": "668d392e2680d7a8ae27cc4f",
+      "impression": "good starting point",
+      "imdb_id": "12312345",
+      "original_title": "jokobo",
+      "primary_title": "jokobo",
+    }
+  ]
+*/
