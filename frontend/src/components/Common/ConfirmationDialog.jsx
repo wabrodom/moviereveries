43426f94e51635378d19e0f2fff0d6fe2a7429d2
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-08-09 08:07:44 +0700

    ConfirmationDialog instead of window.confirm

---
+++ frontend/src/components/Common/ConfirmationDialog.jsx
@@ -0,0 +1,27 @@
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Button from '@mui/material/Button'
+
+const ConfirmationDialog = ({ open, onClose, onConfirm, title, message }) => {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={onConfirm} color="primary" autoFocus>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ConfirmationDialog
